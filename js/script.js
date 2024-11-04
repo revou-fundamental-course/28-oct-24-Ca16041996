81b326a60d1db0de4caef3e07c90f32a5f8dd823
Caesar Lastiko <90476304+Ca16041996@users.noreply.github.com>
2024-11-05 00:20:08 +0700

    current final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 //ini javascript//
+button {
+    function Hasil(){
+        alert("Hasil BMI");
+    }
+    function Reset(){
+        alert("Reset Hasil Hitung BMI");
+    }
+
+    function Download(){
+        alert("Download");
+    }
+
+    function Konsultasi(){
+        alert("Konsultasi")
+    }
+
+    function Regristrasi(){
+        alert("Regristrasi")
+    }
+
+}
